14c41c3c0ea8ebd1bc7702fbe700d3578efe3073
unknown <lappjeff20@gmail.com>
2019-03-06 16:25:28 -0500

    Menu slides in from right but only the first time the menu is click. need to work on slide out

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,15 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open')
+  //GSAP Animations
+  TweenMax.to('.menu', 1.5, {left: '0px'})
+}
+
+const toggleMenuClose = () => {
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle('menu-close')
+  //GSAP Animations
+  // TweenMax.to('.menu', 3, {left: '-350px'})
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -10,3 +19,4 @@ const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu)
+menuButton.addEventListener('click', toggleMenuClose)
