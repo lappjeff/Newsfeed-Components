6a83c3b3c9c288a47ddb8c995dfee894cd10b03a
unknown <lappjeff20@gmail.com>
2019-03-06 19:36:13 -0500

    Articles can now be deleted

--- Article/Article.js
+++ Article/Article.js
@@ -6,16 +6,24 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector('.expandButton');
+
+    this.deleteButton = this.domElement.querySelector('.delete-icon')
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand'
+
+    this.deleteButton.textContent = 'X';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this))
-
+    this.deleteButton.addEventListener('click', this.deleteArticle.bind(this))
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open')
+  }
+
+  deleteArticle() {
+      this.domElement.classList.add('delete')
   }
 
 }
